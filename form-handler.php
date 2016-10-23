db1720649e03ecb4fa8e105c5dabb9a8dbe42c26
Jee Yi <jeeyi@users.noreply.github.com>
2016-10-22 22:26:14 -0700

    basic structure and images

--- form-handler.php
+++ form-handler.php
@@ -18,7 +18,7 @@ if (empty($result)) {
 		$result = $db->query($sql);
 }
 // Set your email below
-$myemail = "ENTER_YOUR_EMAIL_HERE"; // Replace with your email
+$myemail = "acepainting_co@yahoo.com"; 
 
 // Receive and sanitize input
 $name = mysqli_real_escape_string($db, $_POST['name']);
