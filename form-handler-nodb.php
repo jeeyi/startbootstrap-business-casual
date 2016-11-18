37400deb884405ef24db71695d92752ed5203767
Jee Yi <jeeyi@users.noreply.github.com>
2016-11-18 10:20:36 -0800

    email fixed

--- form-handler-nodb.php
+++ form-handler-nodb.php
@@ -11,10 +11,12 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // set up email
-$msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
+$msg = "\n\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\n\n" . $message;
 $msg = wordwrap($msg,70);
-mail($myemail,"New Form Submission",$msg);
-mail($email,"Thank you for your form submission",$msg);
+$mymsg = "Customer has sent you a message!" . $msg;
+$replymsg = "Thank you for your form submission!" . $msg;
+mail($myemail,"Customer has sent you a message",$mymsg);
+mail($email,"Thank you for your form submission",$replymsg);
 echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.';
 
 ?>
